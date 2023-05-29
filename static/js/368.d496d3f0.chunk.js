71f02dd16a5094f7491daf9046c933f1d220bc10
DenShykyta <113693739+DenShykyta@users.noreply.github.com>
2023-05-29 11:26:18 +0000

    Deploying to gh-pages from @ DenShykyta/goit-react-hw-05-movie@fc972bd9af94dd640512e53d614cf0c0dbbf4a5c 🚀

---
+++ static/js/368.d496d3f0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[368],{368:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184),u=function(){return(0,n.jsx)("h1",{children:"Wrong URL. Page NOT FOUND!"})}}}]);
+//# sourceMappingURL=368.d496d3f0.chunk.js.map
